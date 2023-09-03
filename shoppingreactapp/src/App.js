9cacbdbee79b326fc1433a8b6c127df4a830eeb8
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-03 19:24:19 +0400

    Home page left structure added

--- shoppingreactapp/src/App.js
+++ shoppingreactapp/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    
+            <Header/>
+
+            <Routes>
+              <Route path='/' element={  <Home/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+            
+
+           
+            </Routes>
+
+
     
     </BrowserRouter>
   );
